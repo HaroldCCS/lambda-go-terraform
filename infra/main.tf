33e7e70197dd50a6c36a642d7c765cc80df97606
Harold Camargo <hscamargo2002@gmail.com>
2026-04-30 13:31:45 -0500

    fix deploy

--- infra/main.tf
+++ infra/main.tf
@@ -2,24 +2,33 @@ terraform {
   required_version = ">= 1.5.0"
   backend "s3" {
     bucket         = "deploy-lambdas-terraform-state"
-    key            = "infra/terraform.tfstate" # Key diferente para no chocar
+    key            = "infra/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
-provider "aws" { region = "us-east-1" }
+provider "aws" {
+  region = "us-east-1"
+}
 
 # Tabla DynamoDB
 resource "aws_dynamodb_table" "users_table" {
-  name           = "UsersTable"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "userId"
-  attribute { name = "userId"; type = "S" }
+  name         = "UsersTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
 }
 
 # Rol de ejecución para la Lambda
@@ -30,23 +39,32 @@ resource "aws_iam_role" "lambda_exec_shared" {
     Statement = [{
       Action = "sts:AssumeRole"
       Effect = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
     }]
   })
 }
 
-# Permisos: Logs + DynamoDB
+# Permisos: Logs
 resource "aws_iam_role_policy_attachment" "basic_exec" {
   role       = aws_iam_role.lambda_exec_shared.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Política: DynamoDB CRUD
 resource "aws_iam_policy" "dynamo_crud_policy" {
   name = "LambdaDynamoCRUDPolicy"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action   = ["dynamodb:PutItem", "dynamodb:GetItem", "dynamodb:UpdateItem", "dynamodb:DeleteItem", "dynamodb:Scan"]
+      Action = [
+        "dynamodb:PutItem",
+        "dynamodb:GetItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Scan"
+      ]
       Effect   = "Allow"
       Resource = aws_dynamodb_table.users_table.arn
     }]
